63b7cc9416a010e02b2e42f905c96be11fc12d46
benjamin538 <benjamin3122025@gmail.com>
2026-03-05 11:15:26 +0500

    flash on coin

---
+++ src/hooks/CoinPickedUp.cpp
@@ -0,0 +1,49 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
+using namespace geode::prelude;
+
+class IDontKnowHowToNameThis : public CCObject {
+public:
+	void resume(float dt) {
+		FMODAudioEngine::sharedEngine()->resumeAllAudio();
+		CCDirector::get()->getScheduler()->setTimeScale(1.0f);
+		this->release();
+	}
+
+	static IDontKnowHowToNameThis* create() {
+		auto ret = new IDontKnowHowToNameThis();
+		ret->autorelease();
+		ret->retain();
+		return ret;
+	}
+};
+
+class $modify(GJBaseGameLayer) {
+	bool init() {
+		if (!GJBaseGameLayer::init()) return false;
+		return true;
+	}
+
+	void destroyObject(GameObject* obj) {
+		GJBaseGameLayer::destroyObject(obj);
+		if (obj->m_objectID == 1322 || obj->m_objectID == 1329 || obj->m_objectID == 142) {
+			log::info("coin");
+			// freeze
+			CCDirector::get()->getScheduler()->setTimeScale(0.00001f);
+			FMODAudioEngine::sharedEngine()->pauseAllAudio();
+
+			auto bigWhiteFrame = CCSprite::create("square.png");
+			bigWhiteFrame->setScale(500.f);
+			bigWhiteFrame->setZOrder(INFINITY);
+			bigWhiteFrame->setID("a-really-big-white-frame");
+			bigWhiteFrame->setOpacity(200);
+			bigWhiteFrame->setPosition(CCDirector::get()->getWinSize()/2);
+			this->m_uiLayer->addChild(bigWhiteFrame);
+
+			auto idk = IDontKnowHowToNameThis::create();
+			CCDirector::get()->getScheduler()->scheduleSelector(schedule_selector(IDontKnowHowToNameThis::resume), idk, 0.000005f, 0, 0.0f, false);
+			bigWhiteFrame->runAction(CCSequence::create(CCDelayTime::create(0.01f), CCRemoveSelf::create(), nullptr));
+		}
+	}
+};
